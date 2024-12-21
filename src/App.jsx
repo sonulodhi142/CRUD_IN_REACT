3388ef2b3280c6f6e9fa75a6a7bb1a8e300ef714
Sonu Lodhi <sp95759201@gmail.com>
2024-12-21 10:21:47 +0530

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Create from "./components/Create";
+import Read from "./components/Read";
+import Update from "./components/Update";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1>app</h1>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/create" element={<Create/>}/>
+        <Route path="/read/:id" element={<Read/>}/>
+        <Route path="/update/:id" element={<Update/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
