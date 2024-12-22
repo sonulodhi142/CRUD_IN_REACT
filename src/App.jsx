c8e652bd443a30d537692ca879023cb574a595df
Sonu Lodhi <sp95759201@gmail.com>
2024-12-22 22:50:15 +0530

    add bootstrap css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Create from "./components/Create";
 import Read from "./components/Read";
 import Update from "./components/Update";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   return (
