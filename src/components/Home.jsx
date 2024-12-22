170e5aead56f2169e8eeb237de4f2a2b684be464
Sonu Lodhi <sp95759201@gmail.com>
2024-12-22 23:03:03 +0530

    use useEffect hook to fatch the data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 const Home = () => {
+  useEffect(()=>{
+    axios.get('http://localhost:3000/users')
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+  },[])
   return (
-    <div>Home</div>
+    <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
+      <h1>List of User</h1>
+      <div className='w-75 rounder bg-white border shadow p-4'>
+
+      </div>
+    </div>
   )
 }
 
