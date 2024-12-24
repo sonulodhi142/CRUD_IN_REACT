818e279e607817e389a693108a5fb472dcb37faf
Sonu Lodhi <sp95759201@gmail.com>
2024-12-24 22:30:47 +0530

    control the form values using funtions

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -8,24 +8,33 @@ const Create = () => {
     email : "",
     phone : ""
 })
+
+const handleChange = (e) =>{
+  const {name, value} = e.target;
+  setvalues({...values, [name]:value})
+}
+const handleSubmit = (e) =>{
+  e.preventDefault()
+  console.log(values)
+}
   return (
     <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
       <div className='w-50 border bg-white shadow px-5 pt-3 pb-5 rounded'>
         <h1>Add a User</h1>
-        <form >
+        <form onSubmit={handleSubmit} >
           <div className="mb-2">
             <label htmlFor="name">Name:</label>
-            <input type="text" name='name' className='form-control' placeholder='Enter Name' />
+            <input type="text" name='name' onChange={handleChange} value={values.name}  className='form-control' placeholder='Enter Name' />
           </div>
           <div className="mb-2">
             <label htmlFor="email">Email:</label>
-            <input type="text" name='email' className='form-control' placeholder='Enter Email' />
+            <input type="email" name='email' onChange={handleChange} value={values.email}   className='form-control' placeholder='Enter Email' />
           </div>
           <div className="mb-2">
             <label htmlFor="phone">Phone:</label>
-            <input type="text" name='phone' className='form-control' placeholder='Enter Phone' />
+            <input type="number" name='phone' onChange={handleChange} value={values.phone}   className='form-control' placeholder='Enter Phone' />
           </div>
-          <button className='btn btn-success '>Submit</button>
+          <button className='btn btn-success ' type='submit'>Submit</button>
           <Link to='/' className='btn btn-info ms-3'>Back</Link>
         </form>
       </div>
