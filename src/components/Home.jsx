6895e3a0e818a0513f3fff018f15807676d6606c
Sonu Lodhi <sp95759201@gmail.com>
2024-12-25 20:56:07 +0530

    add Navigate function to get value

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,20 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 const Home = () => {
   const [data, setData] = useState([])
+  const navigate = useNavigate()
   useEffect(()=>{
     axios.get('http://localhost:3000/users')
     .then(result => setData(result.data))
     .catch(error => console.log(error));
   },[])
+
+  const Navigate = (id) =>{
+    navigate(`/read/${id}`)
+  }
   return (
     <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
       <h1>List of User</h1>
@@ -36,7 +41,7 @@ const Home = () => {
                   <td>{d.email}</td>
                   <td>{d.phone}</td>
                   <td>
-                    <button className='btn btn-sm btn-info me-2'>View </button>
+                    <button className='btn btn-sm btn-info me-2' onClick={()=>Navigate(d.id)}>View </button>
                     <button className='btn btn-sm btn-primary me-2'>Edit </button>
                     <button className='btn btn-sm btn-danger me-2'>Delete </button>
                   </td>
