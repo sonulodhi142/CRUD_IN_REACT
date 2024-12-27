82508ecb022605af60262a8fe734abd3ec84e4bd
Sonu Lodhi <sp95759201@gmail.com>
2024-12-27 08:20:38 +0530

    complete update component

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,27 +1,45 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 
 
 const Update = () => {
-  const [data, setData] = useState([])
+  const navigate = useNavigate()
+  const [values, setvalues] = useState({
+      name :"",
+      email : "",
+      phone : ""
+  })
   const {id} = useParams()
   useEffect(()=>{
     axios.get('http://localhost:3000/users/'+id)
-    .then(result => setData(result.data))
+    .then(result => setvalues(result.data))
     .catch(error => console.log(error));
   },[])
+
+  const handleChange = (e) =>{
+    const {name, value} = e.target;
+    setvalues({...values, [name]:value})
+  }
+
+  const handleUpdate = (e) =>{
+    e.preventDefault()
+    axios.put('http://localhost:3000/users/'+id, values)
+    .then(result => console.log(result))
+    navigate('/')
+  }
   
   return (
     <div className="d-flex flex-column justify-content-center align-items-center bg-light vh-100">
       <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
         <h1>Update a User</h1>
-        <form >
+        <form onSubmit={handleUpdate}>
           <div className="mb-2">
             <label htmlFor="name">Name:</label>
             <input
-              value={data.name}
+              onChange={handleChange}
+              value={values.name}
               type="text"
               name="name"
               className="form-control"
@@ -31,7 +49,8 @@ const Update = () => {
           <div className="mb-2">
             <label htmlFor="email">Email:</label>
             <input
-            value={data.email}
+              onChange={handleChange}
+              value={values.email}
               type="email"
               name="email"
               className="form-control"
@@ -41,7 +60,8 @@ const Update = () => {
           <div className="mb-2">
             <label htmlFor="phone">Phone:</label>
             <input
-            value={data.phone}
+              onChange={handleChange}
+              value={values.phone}
               type="number"
               name="phone"
               className="form-control"
